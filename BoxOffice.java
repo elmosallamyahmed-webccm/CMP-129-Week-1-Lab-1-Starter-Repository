37cd4e1745a6e1f53150d93b1a63d7231b3d1ab4
elmosallamyahmed-webccm <elmosallamy.ahmed@student.ccm.edu>
2026-09-01 18:23:26 -0400

    Trying to perfect formatting

--- BoxOffice.java
+++ BoxOffice.java
@@ -5,19 +5,18 @@ public class BoxOffice {
         System.out.println("What is the name of the movie?");
         String movie = write.nextLine();
         System.out.println("How many adult tickets were sold?");
-        double AT = write.nextInt();
+        double AT = write.nextDouble();
         System.out.println("How many children tickets were sold?");
-        double CT = write.nextInt();
+        double CT = write.nextDouble();
         double gross = (AT * 10.00) + (CT * 6.00);
         double net = (gross * 0.20);
         double distributorProfit = gross - net;
-        System.out.printf("Movie Name:                                       \"%s\'%n",movie);
-        System.out.printf("Adult Tickets Sold:                               %.2f%n",AT);
-        System.out.printf("Child Tickets Sold:                               %.2f%n",CT);
-        System.out.printf("Gross Box Office Profit:                          $%.2f%n",gross);
-        System.out.printf("Net Box Office Profit:                            $%.2f%n",net);
-        System.out.printf("Amount Paid to Distributor:                       $%.2f%n",distributorProfit);
+        System.out.printf("Movie Name:%20s%n", "\""+ movie + "\"");
+        System.out.printf("Adult Tickets Sold:%20.2f%n",AT);
+        System.out.printf("Child Tickets Sold:%20.2f%n",CT);
+        System.out.printf("Gross Box Office Profit:$%19.2f%n",gross);
+        System.out.printf("Net Box Office Profit:$%19.2f%n",net);
+        System.out.printf("Amount Paid to Distributor:$%19.2f%n",distributorProfit);
         write.close();
     }
-    
-}
+}   
--- InterestEarned.java
+++ InterestEarned.java
@@ -4,12 +4,16 @@ public class InterestEarned {
         Scanner hello = new Scanner(System.in);
         System.out.println("What is the amount in $ currently in your savings account?");
         double principal = hello.nextDouble();
-        System.out.println("What is your interest rate?");
-        double interestRate = hello.nextDouble();
+        System.out.println("What is your interest rate in % form?");
+        double ratePercent = hello.nextDouble();
+        double interestRate = ratePercent/100;
         System.out.println("What is the number of times the interest is compounded?");
         double timesCompounded = hello.nextDouble();
-        
-        
+        double currentAmount = principal + Math.pow((1+(interestRate/timesCompounded)), timesCompounded);
+        double interest = currentAmount - principal;
+        System.out.println("Intereset Rate:                     ");
+        System.out.println(interest);
+        hello.close();
     }
     
 }
